2c65c72632e18c98415ae01ae03979a12bdc2c91
ANDREW REUTTER <andrewreutter@ANDREWs-MacBook.local>
2016-02-13 21:28:52 -0600

    deleting BUILTsrc from git

--- BUILTsrc/handlers/privacy.py
+++
@@ -1,9 +0,0 @@
-from google.appengine.api import users
-
-from IP4ELibs import models, BaseHandler
-
-class MainHandler(BaseHandler.BaseHandler):
-
-    def get(self):
-        self.sendXMLWithCachedFullAndMobileStylesheets(open('BUILTsrc/xml/privacy.xml').read(), 145541695845,
-            'plainFull.xsl', 'plainMobile.xsl')
